d221198d4c9edea305ac02e1b1c6de74d4d4ab15
sherllyking9 <sherllyking4@gmail.com>
2024-11-07 20:07:07 -0600

    arreglando boton carrito

--- home.php
+++ home.php
@@ -5,6 +5,8 @@
     <title>EL RINCON DEL COLECCIONISTA</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <!-- Link de Font Awesome para usar el ícono de carrito -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="./css/layout.css" rel="stylesheet" type="text/css" media="all">
 </head>
 
